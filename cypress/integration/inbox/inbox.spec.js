0f644274c4baeda91ba5728f599305eb8aa4419c
Viktor Maksymenko <viktor.maksymenko@ab-soft.net>
2020-05-22 19:36:21 +0300

    Moved inbox from login folder to separate one

--- cypress/integration/inbox/inbox.spec.js
+++ cypress/integration/inbox/inbox.spec.js
@@ -5,25 +5,24 @@ let taskName;
 let inboxPage;
 
 describe("Inbox tab", function() {
-    
+
     before(function() {
         cy.fixture("../fixtures/users.json").as("users").then((users) => {
             cy.login(users.mainUser);
             cy.visit(Cypress.env('baseAppDevUrl'), {
                 auth: users.serverAuth
-            });      
-        })      
+            });
+        })
     })
 
     describe("CRUD for task", function() {
 
         beforeEach(function() {
             inboxPage = new NavigationBar().openInbox();
-        })
-        
-        it("should create new task", function() {
             taskName = "Task_" + Date.now();
+        })
 
+        it("should create new task", function() {
             inboxPage.addTask(taskName);
             cy.contains(taskName).should('be.visible');
         })
